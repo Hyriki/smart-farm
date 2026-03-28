146090af908d7514759996b1901eeb8444276a6b
Hyriki <realhyriki@gmail.com>
2026-03-28 19:26:15 +0700

    Added the dockerized version of the system

---
+++ src/db/userRepo.ts
@@ -0,0 +1,9 @@
+"use server";
+import { prisma } from "@/lib/prisma";
+
+export async function getUserByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+        where: { email: email }
+    });
+    return user;
+}
