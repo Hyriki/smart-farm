29135190b969a842e104900d1652a039ed16ccc0
Hyriki <realhyriki@gmail.com>
2026-04-07 22:02:21 +0700

    Add Email verfification

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   output: "standalone",
-  allowedDevOrigins: ['initiated-annual-chronic-dayton.trycloudflare.com'],
+  assetPrefix: "localhost:3000",
 };
-
 
 export default nextConfig;
--- src/lib/email/sendMail.ts
+++ src/lib/email/sendMail.ts
@@ -1,23 +1,22 @@
 "use server";
 
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
 import { getVerificationEmailHTML } from "./template";
 
 const localhost = "http://localhost:3000";
 
-function getResendClient() {
-  const apiKey = process.env.RESEND_API_KEY;
-  if (!apiKey) {
-    throw new Error("RESEND_API_KEY is not set");
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_APP_PASSWORD
   }
-  return new Resend(apiKey);
-}
+})
 
 export async function sendVerficationEmail(user: {id: number; email: string; name: string }) {
-    const resend = getResendClient();
     const verificationLink = `${localhost}/utils/verifyEmail/${user.id}`;
-    const emailResult = await resend.emails.send({
-        from: 'onboarding@resend.dev',
+    const emailResult = await transporter.sendMail({
+        from: process.env.GMAIL_USER,
         to: user.email,
         subject: 'Email Verification',
         html: getVerificationEmailHTML(verificationLink, user.name)
